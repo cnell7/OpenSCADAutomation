1f90081c040d5ddb87a3eae980e8588257ac2c11
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-10-11 21:41:52 -0400

    Working

--- keyChainNameTagCreator.bash
+++ keyChainNameTagCreator.bash
@@ -1,19 +1,25 @@
 #! /bin/bash
-cd /Users/christiannell/desktop/research/code
+cd /Users/christiannell/desktop/research/NIRAL/code/second-round
 saveDir="keyChainNameTags"
 openSCAD_file="keyChainTitle.scad"
-nameTagInputDir="Priority"
-
+nameTagInputDir="Priority2"
+subStr=""
+mkdir $saveDir
 cd $nameTagInputDir
 for FILE in *.vtk; 
-	do (if [ "${FILE:0:7}" == "stx_neo" ]; then 
-		echo $FILE | cut -d'-' -f 2
-	elif [ "${FILE:0:4}" == "stx_" ]; then
-		tagName=cut -d'_' -f 2
-		
+	do (
+		if [ "${FILE:0:7}" == "stx_neo" ]; then 
+			pat="([^neo]*)-([^-]*)"
+			[[ $FILE =~ $pat ]]	
+			subStr="${BASH_REMATCH[1]}"
+			subStr=${subStr:1}
+		elif [ "${FILE:0:4}" == "stx_" ]; then
+			pat="([^_]*)-([^-]*)"
+			[[ $FILE =~ $pat ]]	
+			subStr="${BASH_REMATCH[1]}"
+		cd ..
+		/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o "${saveDir}/${subStr}.stl" $openSCAD_file -D "input=\"$subStr\""
+		cd $nameTagInputDir
 	fi
-);done
-
+	);done
 cd ..
-
-mkdir $saveDir
