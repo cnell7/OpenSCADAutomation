4cb8554c450345de7f03742ea125a02d19a34ee0
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-10-12 11:53:56 -0400

    Update keyChainNameTagCreator.bash

--- keyChainNameTagCreator.bash
+++ keyChainNameTagCreator.bash
@@ -1,9 +1,10 @@
-#! /bin/bash
+#!/bin/bash
 cd /Users/christiannell/desktop/research/NIRAL/code/second-round
 saveDir="keyChainNameTags"
 openSCAD_file="keyChainTitle.scad"
 nameTagInputDir="Priority2"
 subStr=""
+declare -a doneTags
 mkdir $saveDir
 cd $nameTagInputDir
 for FILE in *.vtk; 
@@ -13,13 +14,18 @@ for FILE in *.vtk;
 			[[ $FILE =~ $pat ]]	
 			subStr="${BASH_REMATCH[1]}"
 			subStr=${subStr:1}
+			echo $subStr
+			cd ..
+			/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o "${saveDir}/${subStr}.stl" $openSCAD_file -D "input=\"$subStr\"" -D "length=\"7.5\""
 		elif [ "${FILE:0:4}" == "stx_" ]; then
 			pat="([^_]*)-([^-]*)"
 			[[ $FILE =~ $pat ]]	
 			subStr="${BASH_REMATCH[1]}"
-		cd ..
-		/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o "${saveDir}/${subStr}.stl" $openSCAD_file -D "input=\"$subStr\""
+			echo $subStr
+			cd ..
+			/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o "${saveDir}/${subStr}.stl" $openSCAD_file -D "input=\"$subStr\"" -D "length=12"
+			doneTags[\'${subStr}\']=1
+			echo ${doneTags[$subStr]}
+		fi
 		cd $nameTagInputDir
-	fi
 	);done
-cd ..
